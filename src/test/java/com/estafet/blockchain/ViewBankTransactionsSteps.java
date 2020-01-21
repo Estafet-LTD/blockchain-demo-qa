d786685e4a0935c6247511c5c22016f45894df47
Georgi Petkov <georgi.petkov@elsevier.com>
2020-01-21 17:19:45 +0200

    Georgi Petkov: updated bank transaction tests

--- src/test/java/com/estafet/blockchain/ViewBankTransactionsSteps.java
+++ src/test/java/com/estafet/blockchain/ViewBankTransactionsSteps.java
@@ -1,19 +1,47 @@
 package com.estafet.blockchain;
 
+import com.estafet.blockchain.demo.data.lib.bank.Account;
+import com.estafet.blockchain.demo.data.lib.bank.Transaction;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 public class ViewBankTransactionsSteps {
+
+    Account account;
     @Given("This bank account exist:")
     public void setupAccount(DataTable dataTable) {
-
+        Account.deleteAccounts();
+        List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
+        for (int i = 0; i < list.size(); i++) {
+            account = Account.createAccount(list.get(i).get("account name"),list.get(i).get("currency"));
+        }
     }
 
     @Given("The account has the following transactions:")
-    public void setupTransactions(DataTable dataTable) {
-
+    public void setupTransactions(DataTable dataTable) throws Exception{
+        List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).get("transaction").toLowerCase().equals("credit"))
+            {
+                Account.creditAccount(account,Double.valueOf(list.get(i).get("amount")),true);
+            }else if(list.get(i).get("transaction").toLowerCase().equals("debit"))
+            {
+                Account.debitAccount(account,Double.valueOf(list.get(i).get("amount")),true);
+            }else {
+                throw new Exception("Unknown transaction type: "+list.get(i).get("transaction"));
+            }
+        }
     }
 
     @When("The user selects the account (.+)")
@@ -22,17 +50,31 @@ public class ViewBankTransactionsSteps {
     }
 
     @Then("The account balance is (.+)")
-    public void verifyAccountBalance(String string) {
-
+    public void verifyAccountBalance(double expectedBalance) {
+        for (int i=0;i<12;i++) {
+            try {
+                account.transactionClearedWait(account.getId());
+            } catch (RuntimeException e){}
+        }
+        double balance = account.getBalance(account.getId());
+        assertEquals(expectedBalance,balance,0);
     }
 
     @Then("The account status will be (.+)")
     public void verifyAccountStatus(String string) {
-
+        Assert.assertTrue(Account.getLastTransaction(account.getId()).getStatus().equals("CLEARED"));
     }
 
     @Then("User can see the list of transactions")
     public void verifyTransactionsList() {
 
+    }
+    @Test
+    public void testDebitAccount() {
+        Account account =  Account.createCreditedAccount("Shukri Shukriev", "USD",60000);
+        Account.debitAccount(account,250,false);
+        Transaction transaction = Account.getLastTransaction(account.getId());
+        assertEquals(-250.0, transaction.getAmount(),0.1);
+        assertEquals("PENDING", transaction.getStatus());
     }
 }
--- src/test/resources/cucumber/bank_transactions.feature
+++ src/test/resources/cucumber/bank_transactions.feature
@@ -3,18 +3,18 @@ Feature: Bank transactions and details
   Background: List accounts
     Given This bank account exist:
       | account name      | currency |
-      | "Dennis Williams" | "USD"    |
+      | Dennis Williams | USD    |
     And The account has the following transactions:
-      | transaction | amount | status    |
-      | "deposit"   | 1000   | "CLEARED" |
-      | "debit"     | 200    | "CLEARED" |
-      | "debit"     | 300    | "PENDING" |
-    When The user selects the account "Dennis Williams"
+      | transaction | amount |
+      | credit    | 1000   |
+      | debit     | 200    |
+      | debit     | 300    |
+    #When The user selects the account Dennis Williams
 
   Scenario: Validate account name
-    Then The account balance is "500"
-    Then The account status will be "PENDING"
+    Then The account balance is 500
+    Then The account status will be CLEARED
 
-  Scenario: Validate account transactions
-    Then User can see the list of transactions
+  #Scenario: Validate account transactions
+  #  Then User can see the list of transactions
 
