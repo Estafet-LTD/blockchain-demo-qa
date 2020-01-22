91c96af98a19a94b0229de98f4de23db7729723b
Georgi Petkov <georgi.petkov@elsevier.com>
2020-01-22 15:45:17 +0200

    Georgi Petkov: updated bank transfer tests

--- src/test/java/com/estafet/blockchain/BankTransferSteps.java
+++ src/test/java/com/estafet/blockchain/BankTransferSteps.java
@@ -1,31 +1,45 @@
 package com.estafet.blockchain;
 
 
+import com.estafet.blockchain.demo.data.lib.bank.Account;
+import com.estafet.blockchain.demo.data.lib.exchangerate.ExchangeRate;
+import com.estafet.blockchain.demo.data.lib.wallet.Wallet;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.eo.Do;
+import org.junit.Assert;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
 
 
 public class BankTransferSteps {
     @Given("The following wallet exist:")
     public void createWallet(DataTable dataTable) {
-
+        List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
+        Wallet wallet = Wallet.createCreditedWallet(list.get(0).get("wallet name"), BigInteger.valueOf(Long.parseLong(list.get(0).get("balance"))));
+        wallet.setWalletName(list.get(0).get("wallet name"));
+        Assert.assertEquals(wallet.getStatus(),list.get(0).get("balance status"));
     }
 
     @Given("The following bank account exists:")
     public void createAccounts(DataTable dataTable) {
-
+        List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
+        Account.createCreditedAccount(list.get(0).get("account name"),list.get(0).get("currency"),Double.parseDouble(list.get(0).get("account balance")));
     }
 
     @Given("The rate for (.+) is (.+)")
-    public void setExchangeRate(String string, Integer int1) {
-
+    public void setExchangeRate(String currency, Integer rate) {
+        ExchangeRate.setExchangeRate(currency,rate);
     }
 
     @When("(.+) submits a transfer for (.+) to wallet (.+)")
-    public void submitBankTransfer(String string, Integer int1, String string2) {
-
+    public void submitBankTransfer(String from, Integer sum, String to)  throws Exception{
+        Wallet.wallettoWalletTransfer(Account.getAccountByName(from).getWalletAddress(),Account.getAccountByName(to).getWalletAddress(),BigInteger.valueOf(sum),false);
+        Thread.sleep(1000);
     }
 
     @Then("The transaction is in (.+) state")
@@ -34,8 +48,8 @@ public class BankTransferSteps {
     }
 
     @Then("After a while the wallet (.+) is updated with its balance now (.+)")
-    public void balanceUpdate(String string, Integer int1) {
-
+    public void balanceUpdate(String name, Integer balance) {
+        Assert.assertEquals(Wallet.getWallet(Account.getAccountByName(name).getWalletAddress()).getBalance(),(double)balance);
     }
 
     @Then("The transaction in the (.+) account gets (.+)")
@@ -44,8 +58,8 @@ public class BankTransferSteps {
     }
 
     @Then("The account balance of (.+) is now (.+)")
-    public void newAccountBalance(String string, Integer int1) {
-
+    public void newAccountBalance(String name, Integer balance) {
+        Assert.assertEquals(Wallet.getWallet(Account.getAccountByName(name).getWalletAddress()).getBalance(),(double)balance);
     }
 
 }
